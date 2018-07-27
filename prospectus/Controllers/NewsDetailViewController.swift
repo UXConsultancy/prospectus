04e8e69003b36a1ae8bba2362ef7b14b0135571a
Craig Chambers <cchambers@salisbury6c.ac.uk>
2018-07-27 12:13:37 +0100

    Working welcome and news with ALL images downloaded using extension UIImageView (Very sexy!)

--- prospectus/Controllers/NewsDetailViewController.swift
+++ prospectus/Controllers/NewsDetailViewController.swift
@@ -29,18 +29,14 @@ class NewsDetailViewController: UIViewController {
         
         self.title = article?.title
         self.view.backgroundColor = UIColor.white
-        
-        downloadImageFromUrl(url: NSURL(string: (article?.image)!)! as URL)
 
-        detailView.imageView.image = UIImage()
+        detailView.imageView.downloadedFrom(url: URL(string: article.image!)!)
         detailView.dateView.text = article?.date
         detailView.textView.text = article?.text
         
         self.view.addSubview(scrollView)
         scrollView.addSubview(detailView)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(refreshView), name: NSNotification.Name("downloadedImage"), object: nil)
-        
+
         setupViews()
     }
     
@@ -62,40 +58,4 @@ class NewsDetailViewController: UIViewController {
         detailView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
         detailView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
     }
-    
-    
-    func downloadImageFromUrl(url: URL){
-        // Creating a session object with the default configuration.
-        // You can read more about it here https://developer.apple.com/reference/foundation/urlsessionconfiguration
-        let session = URLSession(configuration: .default)
-        
-        // Define a download task. The download task will download the contents of the URL as a Data object and then you can do what you wish with that data.
-        let downloadPicTask = session.dataTask(with: url) { (data, response, error) in
-            // The download has finished.
-            if let e = error {
-                print("Error downloading image: \(e)")
-            } else {
-                // No errors found.
-                // It would be weird if we didn't have a response, so check for that too.
-                if let res = response as? HTTPURLResponse {
-                    print("Downloaded image with response code \(res.statusCode)")
-                    if let imageData = data {
-                        // Finally convert that Data into an image and do what you wish with it.
-                        let image = UIImage(data: imageData)
-                        // Do something with your image.
-                        DispatchQueue.main.async {
-                            self.detailView.imageView.image = image
-                        }
-                    } else {
-                        print("Couldn't get image: Image is nil")
-                    }
-                } else {
-                    print("Couldn't get response code for some reason")
-                }
-            }
-        }
-        
-        downloadPicTask.resume()
-    }
-    
 }
